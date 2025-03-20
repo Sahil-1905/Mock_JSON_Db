9b090acf326db6e53d339865b33193082fce563f
Sahil-1905 <sahilshorey2023@gmail.com>
2025-03-19 18:45:18 -0600

    Update

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+
+//Setup the view engine
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+
+//Middleware
+app.use((req, res, next)=>{
+    const time = new Date();
+
+
+    console.log(time, req.url, req.method);
+
+
+    next();
+});
+
+app.get('/home', (req, res) =>{
+    res.send('This is always triggered secondly');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Connected on port: ${PORT}`);
+});
